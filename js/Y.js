066b4bc1673606a61a464411dc442808043591d1
YUNMEIRENER <897949496@qq.com>
2018-01-20 18:00:37 +0800

    update

---
+++ js/Y.js
@@ -0,0 +1,32 @@
+let citys,weatherobj;
+
+$.ajax({
+    url:"https://www.toutiao.com/stream/widget/local_weather/data/?city=",
+    dataType:"jsonp",
+    success:function(obj){
+        citys = obj.data;
+        console.log(citys);
+        $.getScript("http://int.dpool.sina.com.cn/iplookup/iplookup.php? format=js",function(){
+            getFullWeather(citys.city);
+        });
+    }
+});
+
+
+
+function getFullWeather(nowcity){
+    let cc = $('.now_city');
+    cc.html(nowcity);
+
+    $.ajax({
+        url:"https://www.toutiao.com/stream/widget/local_weather/data/?city="+nowcity,
+        dataType:"jsonp",
+        success:function(obj){
+            weatherobj = obj.data;
+            console.log(weatherobj);
+        }
+    });
+
+
+    $('.now_air_quality').html()
+}
